96370761f690ebb8bf49b5b535d6d34dfc21b58b
maagsauce <meh.kirby@gmail.com>
2019-08-09 18:23:21 -0600

    let's get it bois

--- run.py
+++ run.py
@@ -24,11 +24,15 @@ parser.add_argument("-s", default="hamilton", choices=dict_solver.keys(),
                     help="name of the solver to direct the snake (default: hamilton)")
 parser.add_argument("-m", default="normal", choices=dict_mode.keys(),
                     help="game mode (default: normal)")
+parser.add_argument("-c", default=8, help="column size(default: 8)", type=int)
+parser.add_argument("-r", default=8, help="row size(default: 8)", type=int)
 args = parser.parse_args()
 
 conf = GameConf()
 conf.solver_name = dict_solver[args.s]
 conf.mode = dict_mode[args.m]
+conf.map_rows = args.r
+conf.map_cols = args.c
 print("Solver: %s    Mode: %s" % (conf.solver_name, conf.mode))
 
 Game(conf).run()
